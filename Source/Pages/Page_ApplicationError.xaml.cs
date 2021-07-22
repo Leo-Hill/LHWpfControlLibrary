93c784a3222831cf51408300b7ea810037b44f1d
Leo <Leonhard.Hillmeier@gmx.de>
2021-07-22 17:38:45 +0200

    20210722

---
+++ Source/Pages/Page_ApplicationError.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LHWpfControlLibrary.Source.Pages
+{
+    /***********************************************************************************************
+   * 
+   * This page shows an application/exception error. Can be used for iserting in a Messagebox
+   * 
+   **********************************************************************************************/
+    public partial class Page_ApplicationError : Page
+    {
+        public Page_ApplicationError(Exception qException,ResourceDictionary qRDLanguage, ResourceDictionary qRDTheme)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            //Set the language and the theme
+            this.Resources.MergedDictionaries.Add(qRDLanguage);                       //Set the GUI language
+            this.Resources.MergedDictionaries.Add(qRDTheme);                          //Set the GUI theme
+
+            LExceptionDetails.Content = qException.ToString();
+        }
+    }
+}
